769d74d9a65a9ca53140ec5050ebf6571cfb927e
Correa <tocr@gft.com>
2023-10-23 16:20:03 -0300

    Timer label

--- main.py
+++ main.py
@@ -19,14 +19,24 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+# Window
 window = Tk()
 window.title("Pomodoro")
-window.config(padx = 100, pady = 50)
+window.config(padx = 100, pady = 50, bg = YELLOW)
 
-canvas = Canvas(width = 200, height = 224)
+# Label: "Timer"
+label_01 = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg = GREEN, bg = YELLOW)
+label_01.config(padx=0, pady=5)
+label_01.grid(column=1, row=0)
+
+check_mark = "✔"
+
+
+canvas = Canvas(width = 200, height = 224, bg = YELLOW, highlightthickness=0)
 pomodoro_image = PhotoImage(file = "tomato.png")
-canvas.create_image(103, 112, image = pomodoro_image)
-canvas.create_text(103, 130, text = "00:00", fill = "white", font = ("FONT_NAME", 35, "bold"))
-canvas.pack()
+canvas.create_image(100, 112, image = pomodoro_image)
+canvas.create_text(100, 130, text = "00:00", fill = "white", font = (FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
 
 window.mainloop()
