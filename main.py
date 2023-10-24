4d2655a284a39ffe77e4fd00635249b15629efd2
Correa <tocr@gft.com>
2023-10-24 13:28:10 -0300

    Logic to change timer timespams

--- main.py
+++ main.py
@@ -11,14 +11,19 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(WORK_MIN*60)
+        if reps%2 == 0:
+            count_down(WORK_MIN*60)
+        elif reps%7 == 0:
+            count_down(LONG_BREAK_MIN*60)
+        else:
+            count_down(SHORT_BREAK_MIN*60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(count):
@@ -27,7 +32,10 @@ def count_down(count):
     canvas.itemconfig(timer_text, text = f"{'{:02d}'.format(minutes)}:{'{:02d}'.format(seconds)}")
     if count > 0:
         window.after(1000, count_down, count - 1)
-
+    else:
+        global reps
+        reps += 1
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
